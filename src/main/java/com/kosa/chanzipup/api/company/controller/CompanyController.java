3960a517babbf921df37af67f8341f234b149df3
hyeongil <coguddlf1000@naver.com>
2024-09-26 13:00:11 +0900

    fix: apply category for all companies

--- src/main/java/com/kosa/chanzipup/api/company/controller/CompanyController.java
+++ src/main/java/com/kosa/chanzipup/api/company/controller/CompanyController.java
@@ -49,7 +49,7 @@ public class CompanyController {
     @GetMapping("/list")
     public ResponseEntity<Map<MembershipName, Page<List<CompanyListResponse>>>> getAllCompany(
             @ModelAttribute PageInfo pageInfo,
-            @ModelAttribute CompanySearchCondition searchCondition
+            @ModelAttribute CompanySear chCondition searchCondition
     ) {
         Map<MembershipName, Page<List<CompanyListResponse>>> map = companyQueryService.getAllCompanies(pageInfo.getPage(),
                 pageInfo.getSize(),
