775561d8a8dd51e555065950fb0a1040d49339e5
hyeongil <coguddlf1000@naver.com>
2024-09-26 16:44:58 +0900

    feat: add review for complete estimaterequest

--- src/main/java/com/kosa/chanzipup/domain/membership/MembershipRepository.java
+++ src/main/java/com/kosa/chanzipup/domain/membership/MembershipRepository.java
@@ -1,6 +1,16 @@
 package com.kosa.chanzipup.domain.membership;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+
+    @Query("select m from Membership m "
+            + "left join fetch m.company c "
+            + "left join fetch m.membershipType type "
+            + "where c.email = :email "
+    )
+    List<Membership> findAllByUserEmail(@Param("email") String email);
 }
